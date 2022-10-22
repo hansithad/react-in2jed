5b99d1618c4ce1677e27f8f45b0d9246fcb7ffd8
hansithad <na5703hansitha@gmail.com>
2022-10-22 20:10:45 +0530

    add some styles

--- src/Match.js
+++ src/Match.js
@@ -11,7 +11,11 @@ const Match = (props) => {
   };
 
   return (
-    <div style={{display:'flex','flex-direction': 'column'}}>
+    <div style={{display:'flex','flex-direction': 'column',
+        'padding-bottom':'5px',
+        'box-shadow':
+        "0px 11.6px 6.9px rgba(0, 0, 0, 0.02),0px 16.5px 13.3px rgba(0, 0, 0, 0.028),0px 20.8px 20.6px rgba(0, 0, 0, 0.035),0px 27.2px 30.1px rgba(0, 0, 0, 0.042),0px 38.7px 44.7px rgba(0, 0, 0, 0.05),0px 65px 80px rgba(0, 0, 0, 0.07)"
+        }}>
       <div>
           <h6>Match {matchId}</h6>
       </div>
